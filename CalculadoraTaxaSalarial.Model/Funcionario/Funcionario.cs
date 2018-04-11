b050991ad18fa10787ce66d3482b2ff88fa83eb3
Lucas Pontes <fonseca.pontes@hotmail.com>
2018-04-11 19:44:16 -0300

    Adicionar camada de model e a model do funcionário

---
+++ CalculadoraTaxaSalarial.Model/Funcionario/Funcionario.cs
@@ -0,0 +1,9 @@
+﻿namespace CalculadoraTaxaSalarial.Model
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public double Salario { get; set; }
+    }
+}
